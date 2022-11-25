99f65b60dfd2adbd8917fce126700169d8eae912
f7olivera <f7olivera@gmail.com>
2022-11-25 18:14:45 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import { useState } from "react";
+import { Flex } from "@chakra-ui/react";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import Layout from "./components/Layout";
+import { userData } from "./utils/types";
+
 
 function App() {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(undefined as userData);
 
-  return user ? <div>Profile TBD</div> : <div>Login TBD</div>;
+  return (
+      <Flex justifyContent='center' width='100%' height='100vh' fontFamily='Inter'
+            background={'linear-gradient(-45deg, #4439FF 0%, #AC53FF 100%)'}>
+        <Layout>
+          {user ? <Profile {...user} setUser={setUser} /> : <Login setUser={setUser}/>}
+        </Layout>
+      </Flex>
+  );
 }
 
 export default App;
