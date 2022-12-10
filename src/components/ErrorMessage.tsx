384fe6d456856154c54118f71f12513a690cb1fa
f7olivera <f7olivera@gmail.com>
2022-12-09 23:41:19 -0300

    Replace margin for padding in custom component

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Box } from "@chakra-ui/react";
 
 const ErrorMessage = ({ message }: {message: string | undefined}) => (
-  <Box color='negative.alpha.100' pl={1} fontSize='sm' alignSelf='start' mb={1}>
+  <Box color='negative.alpha.100' pl={1} fontSize='sm' alignSelf='start' pb={1}>
     {message}&nbsp;
   </Box>
 );
