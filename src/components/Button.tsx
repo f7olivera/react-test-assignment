b1bceb3ca21f9aa06b571f2b13e7ced72d68e5ba
f7olivera <f7olivera@gmail.com>
2022-12-10 16:58:31 -0300

    Refactor props parameter

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,20 +2,17 @@ import React from "react";
 import { Button as ChakraButton } from "@chakra-ui/react";
 
 
-function Button({ children, isLoading, onClick }: React.ComponentProps<typeof ChakraButton>) {
+function Button(props: React.ComponentProps<typeof ChakraButton>) {
 
   return (
     <ChakraButton
       type="submit" backgroundColor='accent.alpha.100' color='white' width='75%' gap={2}
       minHeight='3rem' fontWeight='normal' borderRadius='lg' transition='all 0.25s ease-out'
-      disabled={isLoading}
-      onClick={onClick}
       _disabled={{ backgroundColor: 'accent.pressed' }}
       boxShadow='0 8px 24px -4px var(--chakra-colors-accent-alpha-50);'
       _active={{ backgroundColor: 'accent.pressed' }}
-      _hover={{ boxShadow: '0 8px 24px -4px var(--chakra-colors-accent-alpha-100);' }}>
-      {children}
-    </ChakraButton>
+      _hover={{ boxShadow: '0 8px 24px -4px var(--chakra-colors-accent-alpha-100);' }}
+      {...props}/>
   );
 }
 
