27787cb395e8d4f23a3d961bff67a0ee42f0f1d3
f7olivera <f7olivera@gmail.com>
2022-12-10 00:53:52 -0300

    Code formatting

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,24 +3,18 @@ import { Button as ChakraButton } from "@chakra-ui/react";
 import { buttonProps } from "../utils/types";
 
 
-function Button({ children, loading, onClick }: buttonProps) {
+function Button({ children, isLoading, onClick }: buttonProps) {
 
   return (
     <ChakraButton
       type="submit" backgroundColor='accent.alpha.100' color='white' width='75%' gap={2}
-      height='3rem' fontWeight='normal' py='1rem' borderRadius='lg' transition='all 0.25s ease-out'
-      disabled={loading}
+      minHeight='3rem' fontWeight='normal' borderRadius='lg' transition='all 0.25s ease-out'
+      disabled={isLoading}
       onClick={onClick}
-      _disabled={{
-        backgroundColor: 'accent.pressed',
-      }}
+      _disabled={{ backgroundColor: 'accent.pressed' }}
       boxShadow='0 8px 24px -4px var(--chakra-colors-accent-alpha-50);'
-      _active={{
-        backgroundColor: 'accent.pressed',
-      }}
-      _hover={{
-        boxShadow: '0 8px 24px -4px var(--chakra-colors-accent-alpha-100);'
-      }}>
+      _active={{ backgroundColor: 'accent.pressed' }}
+      _hover={{ boxShadow: '0 8px 24px -4px var(--chakra-colors-accent-alpha-100);' }}>
       {children}
     </ChakraButton>
   );
