28c51951b6b6cb70066e64944f302a93ace556c9
f7olivera <f7olivera@gmail.com>
2022-12-10 23:50:50 -0300

    Fix text color

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Box } from "@chakra-ui/react";
 
 const ErrorMessage = ({ message, ...rest } : {message: string | undefined} & React.ComponentProps<typeof Box>) => (
-  <Box color='negative.alpha.100' pl={1} fontSize='sm' alignSelf='start' pb={1} {...rest}>
+  <Box color='var(--chakra-colors-red-100)' pl={1} fontSize='sm' alignSelf='start' pb={1} {...rest}>
     {message}&nbsp;
   </Box>
 );
